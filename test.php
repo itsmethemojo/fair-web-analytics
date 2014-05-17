c1a0d8eb3cf37e3724d508ad1c9b8a1fc6a89643
itsmethemojo <marianpoeschmann@googlemail.com>
2014-05-18 00:17:12 +0200

    removed old test file

--- test.php
+++
@@ -1,7 +0,0 @@
-<img src="http://127.0.0.1/simpletrac/img.php?website=http://itsmethemojo.com/blog/2014/05/13/aus-dem-kompendium-der-prokrastination/" />
-
-
-<iframe style="margin: 0;padding: 0;border: none;width: 50px;height: 50px;" src="http://127.0.0.1/simpletrac/count.php?website=http://itsmethemojo.com/birne.html"/>
-
-
-    
