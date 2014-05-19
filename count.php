7bd7a862627353bd96bb0d11b6526871bfce6a40
itsmethemojo <marianpoeschmann@googlemail.com>
2014-05-19 03:20:13 +0200

    style changes

--- count.php
+++ count.php
@@ -4,7 +4,8 @@ require_once 'config/define.php';
 require_once 'util/checkParameter.php';
 
 $count = $trac->getOverallCount($website);
-echo $count;
+$style= 'padding-right:8px;padding-left:8px;background-color:#9F9F9F;color:white;font-family:Arial, Helvetica, sans-serif;font-weight:bold;font-size:50px;';
+echo '<span style="'.$style.'">'.$count.'</span>';
 exit;
 
 ?>
