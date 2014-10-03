ccb0700d2e1d6a43ee5cceb66b99458d8f5c9ed2
itsmethemojo <marianpoeschmann@googlemail.com>
2014-10-03 13:47:42 +0200

    style fix

--- count.php
+++ count.php
@@ -9,7 +9,7 @@ $overallCount = $trac->getOverallCountForDomain($website);
 <html>
     <head></head>
     <body style="margin:0;">
-        <div style="width:200px;text-align:right;color:#9F9F9F;font-family:Arial, Helvetica, sans-serif;font-weight:bold;">
+        <div style="width:300px;text-align:right;color:#9F9F9F;font-family:Arial, Helvetica, sans-serif;font-weight:bold;">
             <span style="font-size:40px;"><?php echo $count; ?>/</span>
             <span style="font-size:60px;"><?php echo $overallCount; ?></span>
         </div>
