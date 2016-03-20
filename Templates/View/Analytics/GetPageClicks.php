9dbd438082b3be6b2674c6a53041774350174f11
itsmethemojo <marianpoeschmann@googlemail.com>
2016-03-20 23:06:22 +0100

    clear REPO for new structure

--- Templates/View/Analytics/GetPageClicks.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Cache-Control: max-age=900");
-if(isset($this->par["jsonData"])){
-    header('Content-Type: application/json');
-    echo json_encode($this->par["jsonData"]);
-}
-
-
--- autoloader.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once __DIR__.DIRECTORY_SEPARATOR.'Configuration'.DIRECTORY_SEPARATOR.'settings.php';
-include_once __DIR__.DIRECTORY_SEPARATOR.'Classes'.DIRECTORY_SEPARATOR.'Controller'.DIRECTORY_SEPARATOR.'AnalyticsController.php';
-$controller = new AnalyticsController($dbConfig,__DIR__.DIRECTORY_SEPARATOR);
